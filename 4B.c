5977fa363707dda6add35ca21a7c494c6f49ff79
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2023-10-08 14:39:43 +0600

    Add files via upload

---
+++ 4B.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n,m,sum=0;
+    scanf("%d %d", &n, &m);
+    if(n>m) printf("District-1 is in the Red Zone\n");
+    else printf("District-2 is in the Red Zone\n");
+    return 0;
+}
